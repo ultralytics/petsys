416f38578cf2759368cdb332ebf2266ea30c7e6b
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-09-23 12:33:25 -0700

    seaborn plots

--- test_singles.py
+++ test_singles.py
@@ -1,7 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+import os
+import platform
 from copy import copy
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from scipy import stats
+from scipy.optimize import curve_fit
 
 
 def exp1(x, a, b, c):
@@ -18,7 +23,9 @@ def calibrate_energy(x):
     # xspan = np.linspace(px.min(), px.max())
     # plt.plot(px, py, '.')
     # plt.plot(xspan, exp1(xspan, *fit1))
-    return exp1(x, *fit1).clip(max=2000)
+
+    label = '%.3g * exp(%.3g * x) + %.3g' % tuple(fit1)
+    return exp1(x, *fit1).clip(max=2000), label
 
 
 def test_singles(file='my_data_singles.txt'):
@@ -28,7 +35,7 @@ def test_singles(file='my_data_singles.txt'):
 
     # calibrate energy
     e0 = copy(e)
-    x[:, 1] = calibrate_energy(e)  # keV
+    x[:, 1], efit_label = calibrate_energy(e)  # keV
     t, e, ch = x.transpose()  # time (ps), energy (samples), channel
     ecal = copy(e)
 
@@ -44,30 +51,58 @@ def test_singles(file='my_data_singles.txt'):
     dt = np.diff(t)
     a = ch[1:] == 309
     dt[a] = -dt[a]
-    dt = dt[(np.abs(dt) < maxdt) & (np.diff(ch) != 0) & (dt != 0)]
+    dt = dt[(np.abs(dt) < maxdt) & (np.diff(ch) != 0)]
 
-    # plots
+    # Plots
+    plots(x, e0, ecal, dt, efit_label)
+
+
+def plots(x, e0, ecal, dt, efit_label):
+    # matplotlib plots
+    # fig, ax = plt.subplots(2, 3, figsize=(9, 6))
+    # ax = ax.ravel()
+    # ax[0].hist(e0, np.linspace(0, 300, 300))
+    # ax[0].set_xlabel('energy (samples)')
+    # ax[1].hist(ecal, np.linspace(0, 2000, 300))
+    # ax[1].set_xlabel('energy (keV)')
+    # ax[2].hist(dt, np.linspace(-400, 600, 50))
+    # ax[2].set_xlabel('t (ps)')
+    # fig.tight_layout()
+    # # for i in range(3):
+    # #     ax[i].grid()
+    # fig.savefig('results.png', dpi=200)
+
+    # seaborn correlogram
     # import pandas as pd
-    # import seaborn as sns
-    # x = pd.DataFrame(x, columns=['time (ps)', 'E (samples)', 'channel'])
+    # x = pd.DataFrame(x, columns=['time (ps)', 'E (samples)', 'dt'])
     # sns.pairplot(x, corner=True, diag_kind='hist', kind='scatter', markers='o',
     #              plot_kws=dict(s=3, edgecolor=None, linewidth=1, alpha=0.02),
     #              diag_kws=dict(bins=50))
     # plt.savefig('data_correlogram.png', dpi=200)
     # plt.close()
 
-    fig, ax = plt.subplots(2, 3, figsize=(9, 6))
+    # seaborn plots
+    fig, ax = plt.subplots(1, 3, figsize=(12, 4))
     ax = ax.ravel()
-    ax[0].hist(e0, np.linspace(0, 300, 300))
-    ax[0].set_xlabel('energy (samples)')
-    ax[1].hist(ecal, np.linspace(0, 2000, 300))
-    ax[1].set_xlabel('energy (keV)')
-    ax[2].hist(dt, np.linspace(-400, 600, 50))
-    ax[2].set_xlabel('t (ps)')
+    sns.distplot(e0, np.linspace(0, 300, 300), kde=False, axlabel='energy (samples)', ax=ax[0])
+    sns.distplot(ecal, np.linspace(0, 1500, 300), kde=False, axlabel='energy (keV)', ax=ax[1])
+    a = sns.distplot(dt, np.linspace(-400, 600, 50), kde=False, fit=stats.norm, axlabel='time (ps)',
+                     label="Normal fit $\mu=${0:.1f}, $\sigma=${1:.2f}".format(*stats.norm.fit(dt)), ax=ax[2])
+    a.legend()
     fig.tight_layout()
-    # for i in range(3):
-    #     ax[i].grid()
-    fig.savefig('results.png', dpi=200)
+    fig.savefig('time_resolution.png', dpi=200)
+
+    # seaborn energy calibration jointplot
+    import pandas as pd
+    x = pd.DataFrame(np.stack((e0, ecal)).transpose(), columns=['energy (samples)', 'energy (keV)'])
+    a = sns.jointplot(data=x, x='energy (samples)', y='energy (keV)', xlim=[0, 300], ylim=[0, 1500],
+                      joint_kws=dict(gridsize=50), ratio=2, kind='hex', marginal_kws=dict(bins=500))
+    a.ax_joint.text(10, 1400, efit_label)
+    plt.savefig('energy_calibration.png', dpi=200)
+
+    # Display to screen
+    if platform.system() == 'Darwin':  # MacOS
+        os.system('open energy_calibration.png')
 
 
 if __name__ == '__main__':
